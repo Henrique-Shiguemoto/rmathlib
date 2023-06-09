abd4f63b64397ad138dae0d9f9f942540407060c
Rick Shiguemoto <shiguemoto88@gmail.com>
2023-06-09 19:41:52 -0300

    Made a simple assert #define

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mthlib.h"
 
+#define MTHLIB_ASSERT(x) \
+	if(!(x)){ fprintf(stderr, "ASSERT - ERROR AT: (%d)\n", __LINE__); exit(1); }
+
 int main(void){
+	
 	return 0;
 }
